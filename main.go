94915d261e5723cbc441097b84f7d99f40218692
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-10-13 01:09:20 +0000

    简单的Gin示例

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("HelloWorld!")
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(200, "hello!")
+	})
+
+	if err := r.Run(":3000"); err != nil {
+		log.Panicf("failed to start http server: %s", err)
+	}
 }
