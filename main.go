316df9d3a304a11d087a269ad7677ed70f5dc481
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-10-13 01:20:42 +0000

    使用xorm连接mysql

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
 )
+
+var DB *xorm.Engine
+
+func connectDb() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&collation=utf8mb4_unicode_ci",
+		"root",
+		"root",
+		"127.0.0.1",
+		3306,
+		"bandhub_dev",
+	)
+	db, err := xorm.NewEngine("mysql", dsn)
+
+	if err != nil {
+		log.Panicf("falied to connect database: %s", err)
+	}
+
+	log.Println("connected database")
+
+	DB = db
+}
 
 func main() {
 	r := gin.Default()
+
+	connectDb()
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(200, "hello!")
@@ -16,4 +42,6 @@ func main() {
 	if err := r.Run(":3000"); err != nil {
 		log.Panicf("failed to start http server: %s", err)
 	}
+
+	defer DB.Close()
 }
